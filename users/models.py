8d4fa235e1d40f48ef38ea69f30ef6e2a27fcb1b
tansanguy <2t6shg@gmail.com>
2025-09-12 01:45:59 +0900

    chore: make User.role nullable for Kakao login

--- users/models.py
+++ users/models.py
@@ -4,7 +4,8 @@ class User(models.Model):
     kakao_id = models.CharField(max_length=255, unique=True)  # 카카오 로그인 ID
     role = models.CharField(                                # 회원 유형
         max_length=10,
-        choices=[('artist', 'Artist'), ('space', 'Space')]
+        choices=[('artist', 'Artist'), ('space', 'Space')],
+        null=True, blank=True
     )
     # 선행 '0' 보존 + 국가코드 확장 대비
     phone_number = models.CharField(max_length=15, null=True, blank=True)
