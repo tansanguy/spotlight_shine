a58e4a3933d3cef3a2f8cf3cd57d62bde5f15f30
tansanguy <2t6shg@gmail.com>
2025-09-12 13:15:05 +0900

    fix: custom UserAdmin to use kakao_id instead of username

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'kakao_id', 'role', 'phone_number', 'created_at')
-    search_fields = ('kakao_id', 'phone_number')
+class UserAdmin(BaseUserAdmin):
+    ordering = ("id",)
+    list_display = (
+        "id",
+        "kakao_id",
+        "role",
+        "phone_number",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "created_at",
+    )
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("kakao_id", "password")}),
+        (_("개인정보"), {"fields": ("role", "phone_number")}),
+        (_("권한"), {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        (_("중요 날짜"), {"fields": ("last_login", "created_at")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "kakao_id",
+                "password1",
+                "password2",
+                "role",
+                "phone_number",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            ),
+        }),
+    )
+
+    search_fields = ("kakao_id", "phone_number")
